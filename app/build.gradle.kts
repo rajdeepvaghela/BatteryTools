6aa46342ac548a44341f3790be1496542c394845
Rajdeep Vaghela <rajdeep.vaghela610@gmail.com>
2026-04-23 14:17:49 +0530

    Bump version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.rdapps.batterytools"
         minSdk = 34
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
         buildConfigField("String", "DATASTORE_NAME", "\"DataStorePref-v1\"")
     }
 
